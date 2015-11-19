f68865aba74a4260dfddd91fb67ec281762a33ed
Michael Koukoullis <mike@kouky.org>
2015-11-19 23:22:01 +1100

    Boilerplate mac spritekit project

---
+++ Sources/AppDelegate.swift
@@ -0,0 +1,38 @@
+//
+//  AppDelegate.swift
+//  MavlinkPrimaryFlightDisplay
+//
+//  Created by Michael Koukoullis on 19/11/2015.
+//  Copyright (c) 2015 Michael Koukoullis. All rights reserved.
+//
+
+
+import Cocoa
+import SpriteKit
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var skView: SKView!
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        /* Pick a size for the scene */
+        if let scene = GameScene(fileNamed:"GameScene") {
+            /* Set the scale mode to scale to fit the window */
+            scene.scaleMode = .AspectFill
+            
+            self.skView!.presentScene(scene)
+            
+            /* Sprite Kit applies additional optimizations to improve rendering performance */
+            self.skView!.ignoresSiblingOrder = true
+            
+            self.skView!.showsFPS = true
+            self.skView!.showsNodeCount = true
+        }
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication) -> Bool {
+        return true
+    }
+}
