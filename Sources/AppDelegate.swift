248174d78b539440a7fa09ba2248a3d03f443215
Michael Koukoullis <mike@kouky.org>
2015-11-22 13:32:55 +1100

    Initial horizon spritekit node

--- Sources/AppDelegate.swift
+++ Sources/AppDelegate.swift
@@ -17,19 +17,16 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var skView: SKView!
     
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        /* Pick a size for the scene */
-        if let scene = GameScene(fileNamed:"GameScene") {
-            /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .AspectFill
-            
-            self.skView!.presentScene(scene)
-            
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
-            self.skView!.ignoresSiblingOrder = true
-            
-            self.skView!.showsFPS = true
-            self.skView!.showsNodeCount = true
-        }
+        let scene = PrimaryFlightDisplayScene(size: skView.bounds.size)
+        scene.scaleMode = .AspectFill
+        scene.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        skView.presentScene(scene)
+        
+        /* Sprite Kit applies additional optimizations to improve rendering performance */
+        skView.ignoresSiblingOrder = true
+        
+        self.skView.showsFPS = true
+        self.skView.showsNodeCount = true
     }
     
     func applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication) -> Bool {
---
+++ Sources/HorizonNode.swift
@@ -0,0 +1,29 @@
+//
+//  HorizonNode.swift
+//  MavlinkPrimaryFlightDisplay
+//
+//  Created by Michael Koukoullis on 21/11/2015.
+//  Copyright © 2015 Michael Koukoullis. All rights reserved.
+//
+
+import SpriteKit
+
+class HorizonNode: SKNode {
+
+    private let skyNode = SKSpriteNode(color: SKColor.blueColor(), size: CGSize(width: 100, height: 100))
+    private let groundNode = SKSpriteNode(color: SKColor.brownColor(), size: CGSize(width: 100, height: 100))
+    
+    init(size: CGSize) {
+        super.init()
+        skyNode.size = CGSize(width: size.width, height: size.height / 2)
+        groundNode.size = CGSize(width: size.width, height: size.height / 2)
+        skyNode.position = CGPoint(x: 0, y: size.height / 4)
+        groundNode.position = CGPoint(x: 0, y: -size.height / 4)
+        addChild(skyNode)
+        addChild(groundNode)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
