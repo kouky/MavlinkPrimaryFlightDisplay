d2e1071746c3b66418ecab7d2067e0e6096b8be4
Michael Koukoullis <mike@kouky.org>
2016-02-03 23:59:36 +1100

    Consolidate Attitude protocol and struct

--- Sources/Protocols.swift
+++ Sources/Protocols.swift
@@ -12,31 +12,3 @@ protocol SceneType {
     var sceneSize: CGSize { get }
 }
 
-protocol AttitudeType {
-    var rollRadians: Float { get }
-    var pitchRadians: Float { get }
-    
-    func rollAction() -> SKAction
-    func pitchAction(sceneSize sceneSize: CGSize) -> SKAction
-    func pitchReverseAction(sceneSize sceneSize: CGSize) -> SKAction
-}
-
-protocol AttitudeSettable {
-    func setAttitude(attitude: AttitudeType)
-}
-
-extension AttitudeType {
-    func rollAction() -> SKAction {
-        return SKAction.rotateToAngle(CGFloat(rollRadians), duration: 0.05, shortestUnitArc: true)
-    }
-        
-    func pitchAction(sceneSize sceneSize: CGSize) -> SKAction {
-        let displacement = CGFloat(pitchRadians) * -1 * Constants.Angular.pointsPerRadianForSceneSize(sceneSize)
-        return SKAction.moveToY(displacement, duration: 0.05)
-    }
-    
-    func pitchReverseAction(sceneSize sceneSize: CGSize) -> SKAction {
-        let displacement = CGFloat(pitchRadians) * Constants.Angular.pointsPerRadianForSceneSize(sceneSize)
-        return SKAction.moveToY(displacement, duration: 0.05)
-    }
-}
