93a96d98683163916fe92844113caad0bfe31598
Michael Koukoullis <mike@kouky.org>
2016-02-28 20:36:59 +1100

    Cleanup PrimaryFlightDisplay.swift

--- Sources/PrimaryFlightDisplay.swift
+++ Sources/PrimaryFlightDisplay.swift
@@ -7,7 +7,6 @@
 //
 
 import SpriteKit
-import Darwin
 
 public class PrimaryFlightDisplayView: SKView {
     
@@ -126,8 +125,4 @@ extension PrimaryFlightDisplayScene: AttitudeSettable {
         pitchLadder.setAttitude(attitude)
         bankIndicator.setAttitude(attitude)
     }
-}
-
-func delayOnMainQueue(delay: Double, closure: ()->()) {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(delay * Double(NSEC_PER_SEC))), dispatch_get_main_queue(), closure)
 }
