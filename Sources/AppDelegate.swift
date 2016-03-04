d24479cdc10d97697b85fb58eb5d540ffd90fe0d
Michael Koukoullis <mike@kouky.org>
2016-03-04 23:53:20 +1100

    Add custom settings to example

--- Sources/AppDelegate.swift
+++ Sources/AppDelegate.swift
@@ -21,7 +21,28 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(notification: NSNotification) {
         
         let frame = (self.window.contentView?.frame)!
-        let flightView = PrimaryFlightDisplayView(frame: frame)
+        var settings = DefaultSettings()
+        
+        // Customise the settings
+//        let pinkColor = SKColor(red:1.00, green:0.11, blue:0.56, alpha:1.0)
+//        settings.horizon.groundColor = SKColor.brownColor()
+//        
+//        settings.bankIndicator.skyPointerFillColor = pinkColor
+//        settings.bankIndicator.arcMaximumDisplayDegree = 75
+//        settings.bankIndicator.arcRadius = 160
+//        
+//        settings.attitudeReferenceIndex.fillColor = pinkColor
+//        settings.attitudeReferenceIndex.sideBarWidth = 80
+//        settings.attitudeReferenceIndex.sideBarHeight = 15
+//
+//        settings.headingIndicator.pointsPerUnitValue = 8
+//        settings.headingIndicator.size.width = 800
+//        settings.headingIndicator.size.height = 40
+//        settings.headingIndicator.markerTextOffset = 15
+//        settings.headingIndicator.minorMarkerFrequency = 1
+//        settings.headingIndicator.majorMarkerFrequency = 10
+        
+        let flightView = PrimaryFlightDisplayView(frame: frame, settings: settings)
         
         mavlinkController = MavlinkController()
         menuManager = MenuManager(mavlinkMenu: mavlinkMenu, availableSerialPorts: mavlinkController.availableSerialPorts, mavlinkController: mavlinkController)
