78069022918940c2ee5054370a38e969e36adac5
Michael Koukoullis <mike@kouky.org>
2016-03-26 16:04:17 +1100

    Add iOS target

---
+++ Sources/iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MavlinkPrimaryFlightDisplay-iOS
+//
+//  Created by Michael Koukoullis on 26/03/2016.
+//  Copyright © 2016 Michael Koukoullis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
