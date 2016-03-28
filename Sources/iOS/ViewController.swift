30c9326b2bcb9eb279f49f02f2aa3c4e47d4d686
Michael Koukoullis <mike@kouky.org>
2016-03-28 19:28:00 +1100

    Integrate BLE + primary flight display

--- Sources/iOS/ViewController.swift
+++ Sources/iOS/ViewController.swift
@@ -7,14 +7,68 @@
 //
 
 import UIKit
+import ReactiveMavlink
 import PrimaryFlightDisplay
+import CoreBluetooth
 
 class ViewController: UIViewController {
+    
+    let ble = BLE()
+    let reactiveMavlink = ReactiveMavlink()
     
     override func viewDidLoad() {        
         super.viewDidLoad()
         let flightView = PrimaryFlightDisplayView(frame: view.frame)
         flightView.autoresizingMask = [.FlexibleHeight, .FlexibleWidth]
         view.addSubview(flightView)
+        ble.delegate = self
+        
+        reactiveMavlink.headUpDisplay.observeNext { [weak flightView] hud in
+            flightView?.setHeadingDegree(Double(hud.heading))
+            flightView?.setAirSpeed(Double(hud.airSpeed))
+            flightView?.setAltitude(Double(hud.altitude))
+        }
+        
+        reactiveMavlink.attitude.observeNext { [weak flightView] attitude in
+            flightView?.setAttitude(rollRadians: Double(attitude.roll), pitchRadians: Double(attitude.pitch))
+        }
+    }
+}
+
+extension ViewController: BLEDelegate {
+    
+    func bleDidUpdateState(state: CBCentralManagerState) {
+        switch state {
+        case .PoweredOff: break
+        case .PoweredOn:
+            ble.startScanning(200)
+            break
+        case .Resetting: break
+        case .Unauthorized: break
+        case .Unknown: break
+        case .Unsupported: break
+        }
+    }
+    
+    func bleDidDiscoverPeripherals() {
+        if let peripheral = ble.peripherals.first {
+            ble.connectToPeripheral(peripheral)
+        }
+    }
+    
+    func bleDidConnectToPeripheral() {
+    }
+    
+    func bleDidReceiveData(data: NSData?) {
+        if let data = data {
+            reactiveMavlink.receiveData(data)
+        }
+    }
+    
+    func bleDidDisconenctFromPeripheral() {
+    }
+    
+    func bleDidDicoverCharacateristics() {
+        ble.setBaudRate()
     }
 }
